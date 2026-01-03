eee1688812f96aa988b199c58b95318ceb7deb24
dchou1618 <dchou5217@gmail.com>
2026-01-02 22:35:55 -0800

    add randomized collection

--- Algorithms/datastructs/hash_table.py
+++ Algorithms/datastructs/hash_table.py
@@ -1,4 +1,5 @@
 import random
+
 class RandomizedSet:
 
     def __init__(self):
@@ -28,3 +29,38 @@ class RandomizedSet:
         if not self.lst:
             raise IndexError("getRandom() called on empty set")
         return self.lst[random.randint(0, len(self.lst) - 1)]
+    
+class RandomizedCollection:
+
+    def __init__(self):
+        self.table = dict()
+        self.lst = []
+
+    def insert(self, val: int) -> bool:
+        self.lst.append(val)
+        if val not in self.table:
+            self.table[val] = {len(self.lst)-1}
+            return True
+        else:
+            self.table[val].add(len(self.lst)-1)
+            return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.table:
+            idx = self.table[val].pop()
+            if idx != len(self.lst)-1:
+                # not last
+                last = self.lst[-1]
+                self.lst[-1] = self.lst[idx]
+                self.lst[idx] = last
+                self.table[last].remove(len(self.lst)-1)
+                self.table[last].add(idx)
+            self.lst.pop()
+            if not self.table[val]:
+                del self.table[val]
+            return True
+        else:
+            return False
+
+    def getRandom(self) -> int:
+        return self.lst[random.randint(0, len(self.lst)-1)]
