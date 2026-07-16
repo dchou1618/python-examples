4717395509d5a840e8347cc52d1ef2a092d24ef7
dchou1618 <43254117+dchou1618@users.noreply.github.com>
2026-07-15 22:49:17 -0700

    add p49 and test (#53)

---
+++ tests/euler/p49_test.py
@@ -0,0 +1,7 @@
+from Algorithms.numerical_methods.euler.p49 import prime_permutations
+
+def test_sol():
+    assert sorted(prime_permutations(n=4, perms=3)) == ['148748178147', '296962999629']
+
+def test_higher_n():
+    assert sorted(prime_permutations(n=5,perms=4)) == ['83987889379388798837']
