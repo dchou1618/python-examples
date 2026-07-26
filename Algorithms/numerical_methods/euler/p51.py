30d94132e1ae023c0c50652664ce1717354b3d03
dchou1618 <dchou5217@gmail.com>
2026-07-26 00:34:05 -0700

    update correctness
    replace same digit with a new value

--- Algorithms/numerical_methods/euler/p51.py
+++ Algorithms/numerical_methods/euler/p51.py
@@ -1,33 +1,42 @@
 from Algorithms.numerical_methods.euler import utils
-from typing import List, Optional
-import math
+from typing import Optional
+from itertools import combinations
 
-def possible_replacements(n: int, digit: int) -> List[int]:
-    max_digits = math.floor(math.log(n, 10))+1
-    def replace_digit(n: int, digit: int, pos: int):
-        pos_val = (n % (10**pos)) // (10**(pos-1))
-        new_n = n+(digit-pos_val) * (10**(pos-1))
-        if pos == max_digits:
-            return {new_n, n}
-        else:
-            return replace_digit(n, digit, pos+1) | replace_digit(new_n, digit, pos+1)
-    return replace_digit(n, digit, pos=1)
-    
 def prime_digit_replacements(n: int, prime_family: int, start_point: Optional[int] = None) -> Optional[int]:
     """
     n: int 
     prime_family: int
+    return: Smallest prime that has a prime family of size `prime_family`
     """
     is_prime, primes = utils.sieve(n)
+    nums = '0123456789'
     for p in primes:
         if start_point is not None and p < start_point:
             continue
-        for digit in range(1, 10):
-            valid_primes = 0
-            for replacement in possible_replacements(p, digit):
-                if is_prime[replacement]:
-                    valid_primes += 1
-            if valid_primes == prime_family:
-                print(possible_replacements(p, digit))
-                return p
+        s = str(p)
+        for digit in nums:
+            positions = [i for i, c in enumerate(s) if c == digit]
+
+            if not positions:
+                continue
+            for r in range(1, len(positions)+1):
+                for subset in combinations(positions, r):
+                    if subset[-1] == n-1:
+                        continue
+                    count = 0
+                    smallest = None
+                    for replacement in nums:
+                        if subset[0] == 0 and replacement == "0":
+                            continue
+                        updated = list(s)
+                        for i in subset:
+                            updated[i] = replacement
+
+                        candidate = int(''.join(updated))
+                        if is_prime[candidate]:
+                            count += 1
+                            if smallest is None or candidate < smallest:
+                                smallest = candidate
+                    if count == prime_family:
+                        return smallest
     return None
